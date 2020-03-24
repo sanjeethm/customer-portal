5ec5a4b5dbf11310d8894ecfd48b9b68ecb860e1
Sanjeeth Kumar Mandal <sanjeeth@sg.ibm.com>
2020-03-24 17:39:33 +0800

    first commit

---
+++ src/main/java/com/san/customerportal/CustomerController.java
@@ -0,0 +1,14 @@
+package com.san.customerportal;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class CustomerController {
+	@GetMapping
+	public String get() {
+		return "We good";
+	}
+}
