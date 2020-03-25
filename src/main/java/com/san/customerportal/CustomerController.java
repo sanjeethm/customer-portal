6d87aa9323e761f515a820d02f2cc7f4a8e6f7ff
sanjeethm <35677742+sanjeethm@users.noreply.github.com>
2020-03-25 17:58:17 +0800

    Update CustomerController.java

--- src/main/java/com/san/customerportal/CustomerController.java
+++ src/main/java/com/san/customerportal/CustomerController.java
@@ -1,33 +1,14 @@
 package com.san.customerportal;
 
-import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 @RestController
 @CrossOrigin
 public class CustomerController {
-	private CustomerRepository customerRepository;
-
-	public CustomerController(CustomerRepository customerRepository) {
-		this.customerRepository = customerRepository;
-	}
-
-	@PostMapping
-	public Customer save(@RequestParam String firstName,
-						 @RequestParam String lastName,
-						 @RequestParam Integer age) {
-		final Customer customer = new Customer().setFirstName(firstName).setLastName(lastName).setAge(age);
-		return customerRepository.save(customer);
-	}
-
-	@GetMapping("{id}")
-	public Customer getOne(@PathVariable Long id) {
-		final Customer jpaCustomer = customerRepository.getOne(id);
-		// wrapping in a new Customer object b/c the JPA implementation may not be serializable
-		return new Customer()
-				.setAge(jpaCustomer.getAge())
-				.setLastName(jpaCustomer.getLastName())
-				.setFirstName(jpaCustomer.getFirstName())
-				.setId(jpaCustomer.getId());
+	@GetMapping
+	public String get() {
+		return "We good";
 	}
 }
 
